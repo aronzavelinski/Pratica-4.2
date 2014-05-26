8a901125613a427e0234257411b16726031e4855
Wilson <Wilson@200.134.10.158>
2014-05-26 12:09:22 -0300

    Correções estéticas.

--- src/main/java/ExemploInterface.java
+++ src/main/java/ExemploInterface.java
@@ -7,9 +7,8 @@ import utfpr.ct.dainf.if62c.exemplos.TrianguloEquilatero;
  * IF62C Fundamentos de Programação 2
  * Exercício de programação em Java.
  * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
- */public class ExemploInterface 
-{
-    public static void main( String[] args )
+ */public class ExemploInterface {
+    public static void main(String[] args)
     {
         Retangulo r = new Retangulo(4, 2);
         Quadrado q = new Quadrado(4);
